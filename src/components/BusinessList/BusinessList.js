92542224fc45f8cacea852c5507e1536934b0c1a
Luci Temple <lucitemple@yahoo.com.au>
2021-04-22 23:37:40 +1000

    Add BusinessList component

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './BusinessList.css';
+import Business from '../Business/Business';
+
+export default class BusinessList extends React.Component {
+    render(){
+        return (
+            <div className="BusinessList">
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+<Business />
+</div>
+        )
+    }
+
+}
+
