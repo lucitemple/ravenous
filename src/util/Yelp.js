337a26b6e059863fbd024bd2d73fc63f4cbd6bfc
Luci Temple <lucitemple@yahoo.com.au>
2021-04-28 11:33:49 +1000

    troubleshoot cors-anywhere added

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -6,9 +6,9 @@ export const Yelp = {
   search(term, location, sortBy) {
     // temporarily bypass CORS restrictions https://cors-anywhere.herokuapp.com/corsdemo
     return fetch(
-      `https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
       {
-/*         method: "GET",
+        /*         method: "GET",
         mode: "no-cors", */
         headers: {
           Authorization: `Bearer ${apiKey}`,
